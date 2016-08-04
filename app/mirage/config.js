238f60488bc9de2b77b0d800a4ac3f157dc80ea4
Kevin Ansfield <kevin@lookingsideways.co.uk>
2016-08-04 14:17:34 +0200

    use an Invite model instead of overloading the User model

--- app/mirage/config.js
+++ app/mirage/config.js
@@ -69,6 +69,54 @@ function paginatedResponse(modelName, allModels, request) {
     };
 }
 
+function mockInvites(server) {
+    server.get('/invites/', function (db, request) {
+        let response = paginatedResponse('invites', db.invites, request);
+        return response;
+    });
+
+    server.get('/invites/:id', function (db, request) {
+        let {id} = request.params;
+        let invite = db.invites.find(id);
+
+        if (!invite) {
+            return new Mirage.Response(404, {}, {
+                errors: [{
+                    errorType: 'NotFoundError',
+                    message: 'Invite not found.'
+                }]
+            });
+        } else {
+            return {invites: [invite]};
+        }
+    });
+
+    server.post('/invites/', function (db, request) {
+        let [attrs] = JSON.parse(request.requestBody).invites;
+        let invite;
+
+        attrs.token = `${db.invites.length}-token`;
+        attrs.expires = moment.utc().add(1, 'day').unix();
+        attrs.created_at = moment.utc().format();
+        attrs.created_by = 1;
+        attrs.updated_at = moment.utc().format();
+        attrs.updated_by = 1;
+        attrs.status = 'sent';
+
+        invite = db.invites.insert(attrs);
+
+        return {
+            invite
+        };
+    });
+
+    server.del('/invites/:id/', function (db, request) {
+        db.invites.remove(request.params.id);
+
+        return new Mirage.Response(204, {}, {});
+    });
+}
+
 function mockSubscribers(server) {
     server.get('/subscribers/', function (db, request) {
         let response = paginatedResponse('subscribers', db.subscribers, request);
@@ -144,6 +192,8 @@ export default function () {
 
     // keep this line, it allows all other API requests to hit the real server
     this.passthrough();
+
+    mockInvites(this);
 
     // add any external domains to make sure those get passed through too
     this.passthrough('https://count.ghost.org/');
